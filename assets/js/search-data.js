eda68848e76d459c10c8a71ece1280585fe51566
fatlock1712 <fatlock17122007@gmail.com>
2026-06-09 21:44:46 +0000

    Deploying to gh-pages from @ fatlock1712/fatlock1712.github.io@e8a03a992f2f5aa6d424cd8bf3f9b2e7a9433a82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,11 +384,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-published-my-own-website",
           title: 'Published my own website.',
           description: "",
